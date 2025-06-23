0415286502f1014f8531f5bee5b0c432fb840ee8
Oracle <anothermetomotheryou@gmail.com>
2025-06-23 15:39:47 -0400

    Moved proposal modal to interactionCreate

--- commands/resolutions/propose.js
+++ commands/resolutions/propose.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, MessageFlags } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, MessageFlags } = require('discord.js');
 const { peerId } = require('../../config.json');
 
 module.exports = {
@@ -11,7 +11,7 @@ module.exports = {
 
             // Build the menu for selecting the class of proposal
             const proposal_select = new StringSelectMenuBuilder()
-                .setCustomId("proposal_select")
+                .setCustomId("proposalSelect")
                 .setPlaceholder("Make a selection!")
                 .addOptions(
                     new StringSelectMenuOptionBuilder()
@@ -63,26 +63,7 @@ module.exports = {
                     flags: MessageFlags.Ephemeral
                 });
 
-                // Await a response
-                const collectorFilter = i => i.user.id === interaction.user.id;
-                try {
-                    const confirmation = await response.resource.message.awaitMessageComponent({ filter: collectorFilter, time: 3_600_000 });
-
-                    // Check if the proposal is an amendment of an official document
-                    if (confirmation.values[0] === "amd_admin" || confirmation.values[0] === "amd_rp" || confirmation.values[0] === "amd_format" || confirmation.values[0] === "amd_community") {
-                        // Now generate an appropriate modal 
-
-                    // Check if the proposal is an application of membership or peerage
-                    } else if (confirmation.values[0] === "app_member" || confirmation.values[0] === "app_peer") {
-                        // Now generate an appropriate modal
-
-                    }
-
-
-                } catch {
-                    // User waits too long
-                    await interaction.editReply({ content: "Confirmation not received. Try running the command again. `/propose`", components: [] });
-                }
+                // The response is detected by interactionCreate.js
          } else {
             // User is not a peer
             await interaction.reply({ content: "No permission.", flags: MessageFlags.Ephemeral });
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,4 @@
-const { Events, MessageFlags } = require('discord.js');
+const { Events, MessageFlags, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
 
 module.exports = {
 	name: Events.InteractionCreate,
@@ -17,12 +17,49 @@ module.exports = {
 			} catch (error) {
 				console.error(error);
 			}
-
+			
+		// check for modal submissions
 		} else if (interaction.isModalSubmit()) {
-			if (interaction.customId === 'resolutionModal') {
+			if (interaction.customId === 'amendmentModal') {
 				console.log("Received a resolution.");
 				await interaction.reply({ content: "Your submission has been added to the queue."});
 			}		
+		} else if (interaction.isStringSelectMenu()) {
+			if (interaction.customId === "proposalSelect") {
+				console.log(interaction.values[0]);
+				// Build a modal based on the selection
+
+				//If the selection was an amendment of an official document
+				if (interaction.values[0] === "amd_admin" || interaction.values[0] === "amd_rp" || interaction.values[0] === "amd_format" || interaction.values[0] === "amd_community") {
+                        const modal = new ModalBuilder()
+							.setCustomId('amendmentModal')
+							.setTitle('Amendment of an Official Document');
+
+						const amendmentSummaryInput = new TextInputBuilder()
+							.setCustomId('amendmentSummary')
+							.setLabel("Summary")
+							.setStyle(TextInputStyle.Paragraph);
+
+						const amendmentDetailsInput = new TextInputBuilder()
+							.setCustomId('amendmentDetails')
+							.setLabel("Details")
+							.setStyle(TextInputStyle.Paragraph);
+
+						const amendmentFirstRow = new ActionRowBuilder().addComponents(amendmentSummaryInput);
+						const amendmentSecondRow = new ActionRowBuilder().addComponents(amendmentDetailsInput);
+
+						// Add inputs to the modal
+						modal.addComponents(amendmentFirstRow, amendmentSecondRow);
+
+						// Show the modal to the user
+						await interaction.showModal(modal);
+
+                    // Check if the proposal is an application of membership or peerage
+                    } else if (interaction.values[0] === "app_member" || interaction.values[0] === "app_peer") {
+                        // Now generate an appropriate modal
+
+                    }
+			}
 		}
 	},
 };
