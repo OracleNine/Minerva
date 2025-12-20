b7e4e8cef4e8e638104e8b32216c0511e0a9a4a2
Oracle <15791794+OracleNine@users.noreply.github.com>
2025-12-20 00:43:32 -0500

    Never mind

--- prod/test.js
+++ prod/test.js
@@ -1,53 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let text = `
-He has affected to render the Military independent of and superior to the Civil power.
++ The unanimous Declaration of the thirteen united States of America, When in the Course of human events, it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature’s God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation.
 
-He has combined with others to subject us to a jurisdiction foreign to our constitution, and unacknowledged by our laws; giving his Assent to their Acts of pretended Legislation:
+- But when a long train of abuses and usurpations, pursuing invariably the same Object evinces a design to reduce them under absolute Despotism, it is their right, it is their duty, to throw off such Government, and to provide new Guards for their future security.–Such has been the patient sufferance of these Colonies; and such is now the necessity which constrains them to alter their former Systems of Government. The history of the present King of Great Britain is a history of repeated injuries and usurpations, all having in direct object the establishment of an absolute Tyranny over these States. To prove this, let Facts be submitted to a candid world. We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness.–That to secure these rights, Governments are instituted among Men, deriving their just powers from the consent of the governed, –That whenever any Form of Government becomes destructive of these ends, it is the Right of the People to alter or to abolish it, and to institute new Government, laying its foundation on such principles and organizing its powers in such form, as to them shall seem most likely to effect their Safety and Happiness. Prudence, indeed, will dictate that Governments long established should not be changed for light and transient causes; and accordingly all experience hath shewn, that mankind are more disposed to suffer, while evils are sufferable, than to right themselves by abolishing the forms to which they are accustomed. But when a long train of abuses and usurpations, pursuing invariably the same Object evinces a design to reduce them under absolute Despotism, it is their right, it is their duty, to throw off such Government, and to provide new Guards for their future security.–Such has been the patient sufferance of these Colonies; and such is now the necessity which constrains them to alter their former Systems of Government. The history of the present King of Great Britain is a history of repeated injuries and usurpations, all having in direct object the establishment of an absolute Tyranny over these States. 
 
-For Quartering large bodies of armed troops among us:
+- He has refused his Assent to Laws, the most wholesome and necessary for the public good.
 
-For protecting them, by a mock Trial, from punishment for any Murders which they should commit on the Inhabitants of these States:
-
-For cutting off our Trade with all parts of the world:
-
-For imposing Taxes on us without our Consent:
-
-For depriving us in many cases, of the benefits of Trial by Jury:
-
-For transporting us beyond Seas to be tried for pretended offences:
-
-For abolishing the free System of English Laws in a neighbouring Province, establishing therein an Arbitrary government, and enlarging its Boundaries so as to render it at once an example and fit instrument for introducing the same absolute rule into these Colonies:
-
-For taking away our Charters, abolishing our most valuable Laws, and altering fundamentally the Forms of our Governments:
-
-For suspending our own Legislatures, and declaring themselves invested with power to legislate for us in all cases whatsoever.
-
-He has abdicated Government here, by declaring us out of his Protection and waging War against us.
-
-He has plundered our seas, ravaged our Coasts, burnt our towns, and destroyed the lives of our people.
-
-He is at this time transporting large Armies of foreign Mercenaries to compleat the works of death, desolation and tyranny, already begun with circumstances of Cruelty & perfidy scarcely paralleled in the most barbarous ages, and totally unworthy the Head of a civilized nation.
-
-He has constrained our fellow Citizens taken Captive on the high Seas to bear Arms against their Country, to become the executioners of their friends and Brethren, or to fall themselves by their Hands.
-
-He has excited domestic insurrections amongst us, and has endeavoured to bring on the inhabitants of our frontiers, the merciless Indian Savages, whose known rule of warfare, is an undistinguished destruction of all ages, sexes and conditions.
-
-In every stage of these Oppressions We have Petitioned for Redress in the most humble terms: Our repeated Petitions have been answered only by repeated injury. A Prince, whose character is thus marked by every act which may define a Tyrant, is unfit to be the ruler of a free people.
-
-Nor have We been wanting in attentions to our Brittish brethren. We have warned them from time to time of attempts by their legislature to extend an unwarrantable jurisdiction over us. We have reminded them of the circumstances of our emigration and settlement here. We have appealed to their native justice and magnanimity, and we have conjured them by the ties of our common kindred to disavow these usurpations, which, would inevitably interrupt our connections and correspondence. They too have been deaf to the voice of justice and of consanguinity. We must, therefore, acquiesce in the necessity, which denounces our Separation, and hold them, as we hold the rest of mankind, Enemies in War, in Peace Friends.
-
-We, therefore, the Representatives of the united States of America, in General Congress, Assembled, appealing to the Supreme Judge of the world for the rectitude of our intentions, do, in the Name, and by Authority of the good People of these Colonies, solemnly publish and declare, That these United Colonies are, and of Right ought to be Free and Independent States; that they are Absolved from all Allegiance to the British Crown, and that all political connection between them and the State of Great Britain, is and ought to be totally dissolved; and that as Free and Independent States, they have full Power to levy War, conclude Peace, contract Alliances, establish Commerce, and to do all other Acts and Things which Independent States may of right do. And for the support of this Declaration, with a firm reliance on the protection of divine Providence, we mutually pledge to each other our Lives, our Fortunes and our sacred Honor.
++ He has forbidden his Governors to pass Laws of immediate and pressing importance, unless suspended in their operation till his Assent should be obtained; and when so suspended, he has utterly neglected to attend to them.
 `;
+function lastIndexOfRegex(expression, target) {
+    let lastIndex = -1;
+    for (const match of target.matchAll(expression)) {
+        lastIndex = match.index;
+    }
+    return lastIndex + 1;
+}
 while (text.matchAll(/^[+-][^\r\n]{1000,}/gmd).next().value !== undefined) {
     let runOnResult = text.matchAll(/^[+-][^\r\n]{1000,}/gmd).next().value;
     let runOnCoordinates = runOnResult.indices[0];
     let runOn = text.substring(runOnCoordinates[0], runOnCoordinates[1]);
-    let toFirstPunctuation = runOn.matchAll(/^[+-].{0,1000}[\.|\?|\!]/gmd);
-    if (toFirstPunctuation === null) {
-        toFirstPunctuation = runOn.matchAll(/^[+-].{0,1000}/gmd);
-    }
-    let toPunctuationEndCoord = toFirstPunctuation.next().value.indices[0][1];
+    let endCoord = lastIndexOfRegex(/\.|\!|\?/gm, runOn);
     let plusOrMinus = "\n";
     if (runOn.match(/^[+]/g) !== null) {
         plusOrMinus += "+";
@@ -55,32 +28,7 @@ while (text.matchAll(/^[+-][^\r\n]{1000,}/gmd).next().value !== undefined) {
     else {
         plusOrMinus += "-";
     }
-    text = text.slice(0, runOnCoordinates[0] + toPunctuationEndCoord) + plusOrMinus + text.slice(runOnCoordinates[0] + toPunctuationEndCoord);
+    text = text.slice(0, runOnCoordinates[0] + endCoord) + plusOrMinus + text.slice(runOnCoordinates[0] + endCoord);
+    console.log(text.matchAll(/^[+-][^\r\n]{1000,}/gmd).next().value);
 }
-function lastIndexOfRegex(expression, target) {
-    let lastIndex = -1;
-    for (const match of target.matchAll(expression)) {
-        lastIndex = match.index;
-    }
-    return lastIndex;
-}
-let returnChunks = [];
-while (text.length > 1800) {
-    let toCharLimit = text.substring(0, 1800);
-    let chunkEnd = toCharLimit.lastIndexOf("\n");
-    if (chunkEnd <= 0) {
-        chunkEnd = lastIndexOfRegex(/\.|\!|\?/gm, toCharLimit);
-        if (chunkEnd <= 0) {
-            chunkEnd = lastIndexOfRegex(/\s/gm, toCharLimit);
-            if (chunkEnd <= 0) {
-                chunkEnd = 1800;
-            }
-        }
-    }
-    let newChunk = text.substring(0, chunkEnd);
-    returnChunks.push(newChunk);
-    text = text.slice(chunkEnd);
-}
-returnChunks.push(text);
-console.log(returnChunks);
 //# sourceMappingURL=test.js.map
